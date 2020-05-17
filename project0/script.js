28e4c07bac29008d60fe41aa4cbcc20bbb43bb72
imaHungryPanda <stevelolsen@gmail.com>
2020-05-17 10:13:57 -0500

    [feature] Complete base of assignment
    Create a basic todo list.

--- project0/script.js
+++ project0/script.js
@@ -1,14 +1,47 @@
 const classNames = {
-  TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-checkbox',
-  TODO_TEXT: 'todo-text',
-  TODO_DELETE: 'todo-delete',
+  TODO_ITEM: "todo-container",
+  TODO_CHECKBOX: "todo-checkbox",
+  TODO_TEXT: "todo-text",
+  TODO_DELETE: "todo-delete",
 }
 
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
+const list = document.getElementById("todo-list")
+const itemCountSpan = document.getElementById("item-count")
+const uncheckedCountSpan = document.getElementById("unchecked-count")
 
-function newTodo() {
-  alert('New TODO button clicked!')
+const newTodo = () => {
+  const checkbox = createDomElement('input',
+    {
+      type: 'checkbox',
+      class: classNames.TODO_CHECKBOX,
+      onclick: 'updateUncheckedCount()',
+    }
+  )
+  const todoInput = createDomElement('input', { class: classNames.TODO_TEXT })
+  const newTodo = createDomElement("li", { class: classNames.TODO_ITEM })
+
+  newTodo.appendChild(checkbox)
+  newTodo.appendChild(todoInput)
+  list.appendChild(newTodo)
+
+  updateTotalCount()
+}
+
+const updateTotalCount = () => {
+  itemCountSpan.innerText = list.childNodes.length
+  updateUncheckedCount()
+}
+
+const updateUncheckedCount = () => {
+  uncheckedCountSpan.innerText = document.querySelectorAll("input[type='checkbox']:not(:checked)").length
+}
+
+const createDomElement = (tag, options = {}) => {
+  const newElement = document.createElement(tag)
+
+  Object.keys(options).forEach(option => {
+    newElement.setAttribute(option, options[option])
+  })
+
+  return newElement
 }
